995cdacdbcf8d982809ecfd34df5c4734ebc35c6
Alexandre LAM <alexandrelam@outlook.com>
2024-08-05 21:46:55 +0200

    replace image with main

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="slidev-layout">
         <slot />
-        <img src="https://github.com/alexandrelam/slidev-theme-doctolib/blob/new-theme/public/logo-blue.png?raw=true" class="h-4 absolute top-125 left-1/2 -translate-x-1/2" />
+        <img src="https://github.com/alexandrelam/slidev-theme-doctolib/blob/main/public/logo-blue.png?raw=true" class="h-4 absolute top-125 left-1/2 -translate-x-1/2" />
     </div>
 </template>
